8ce0b7d4899172b4591a7d78d38b0769a1cd53f8
Daniel-Dief <danieldiefsantos@gmail.com>
2026-08-26 17:08:06 -0300

    feat: implementacao inicial lib de api

--- api/go.mod
+++ api/go.mod
@@ -4,5 +4,8 @@ go 1.27
 
 require (
 	github.com/joho/godotenv v1.5.1
+	github.com/labstack/echo/v5 v5.3.1
 	github.com/lib/pq v1.12.3
 )
+
+require golang.org/x/time v0.15.0 // indirect
--- api/main.go
+++ api/main.go
@@ -3,21 +3,36 @@ package main
 import (
 	"finance-control-api/database"
 	"fmt"
-	"log"
+	"net/http"
 
 	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
 )
 
 func main() {
 	err := godotenv.Load("../.env")
 	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
+		panic("Error loading .env file: " + err.Error())
 	}
 	fmt.Println("Environment variables loaded successfully.")
 
 	ping, err := database.PingDatabase()
 	if err != nil {
-		log.Fatalf("Error pinging the database: %v", err.Error())
+		panic("Error pinging the database: " + err.Error())
 	}
 	fmt.Println(ping)
+
+	e := echo.New()
+
+	e.Use(middleware.RequestLogger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c *echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{"message": "Hello, World!"})
+	})
+
+	if err := e.Start(":8080"); err != nil {
+		e.Logger.Error("Failed to start server: ", "error", err)
+	}
 }
